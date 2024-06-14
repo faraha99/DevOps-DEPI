37366991d135810b03a23732135a23e6dd9928ac
faraha99 <105754529+faraha99@users.noreply.github.com>
2024-06-14 16:33:08 +0300

    app.py

--- app.py
+++ app.py
@@ -1,47 +1,55 @@
-import os
-from datetime import datetime
-
-import pytz
+from flask import Flask, request, render_template, jsonify
 import requests
-from dotenv import load_dotenv
-from flask import Flask, render_template
+import pytz
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
-load_dotenv('.env')
 
+API_KEY = 'bad4c8cff47886ca6d92626fbee32d8e'
+COUNTRIES = {
+    'Egypt': 'Cairo',
+    'India': 'Delhi',
+    'Japan': 'Tokyo',
+    'Brazil': 'Sao Paulo',
+    'Australia': 'Sydney',
+    'France': 'Paris',
+    'Canada': 'Toronto',
+    'Germany': 'Berlin',
+    'South Africa': 'Cape Town'
+}
 
-@app.route('/')
-def home():
-    # Get current time in Cairo, Egypt
-    egypt_tz = pytz.timezone('Africa/Cairo')
-    egypt_time = datetime.now(egypt_tz)
+def get_weather(city):
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+    response = requests.get(url)
+    weather_data = response.json()
+    return weather_data
 
-    # Format the time as string
-    formatted_time = egypt_time.strftime('%Y-%m-%d %H:%M %p')
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        country = request.form['country']
+        city = COUNTRIES.get(country)
+        if city:
+            weather_data = get_weather(city)
+            country_code = next((code for code, name in pytz.country_names.items() if name == country), None)
+            if country_code:
+                time_zone = pytz.timezone(pytz.country_timezones[country_code][0])
+                current_time = datetime.now(time_zone)
+                adjusted_time = current_time - timedelta(hours=1)
+                data = {
+                    'country': country,
+                    'city': city,
+                    'current_time': adjusted_time.strftime('%Y-%m-%d %I:%M:%S %p'),
+                    'time_zone': time_zone.zone,
+                    'weather': {
+                        'description': weather_data['weather'][0]['description'],
+                        'temperature': weather_data['main']['temp']
+                    }
+                }
+                return render_template('index.html', data=data, countries=COUNTRIES.keys())
+    return render_template('index.html', countries=COUNTRIES.keys())
 
-    # Get weather data for Cairo, Egypt.
-    api_key = os.getenv('API_KEY')
-    weather_url = f'http://api.openweathermap.org/data/2.5/weather?q=Cairo,EG&units=metric&appid={api_key}'
-    weather_response = requests.get(weather_url)
-    weather_data = weather_response.json()
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
-    # Extract weather description and temperature if available
-    if 'weather' in weather_data:
-        weather_description = weather_data['weather'][0]['description']
-    else:
-        weather_description = 'Weather data unavailable'
-
-    if 'main' in weather_data:
-        temperature = weather_data['main']['temp']
-    else:
-        temperature = 'Temperature data unavailable'
-
-    return render_template('index.html',
-                           current_time=formatted_time,
-                           time_zone='Africa/Cairo',
-                           weather_description=weather_description,
-                           temperature=temperature)
-
-
-if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
