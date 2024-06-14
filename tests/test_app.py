978bd230f43b1e9459312eead7a77a09770e5098
faraha99 <105754529+faraha99@users.noreply.github.com>
2024-06-14 16:35:23 +0300

    test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,19 +1,27 @@
-import json
-
+import sys
+import os
 import pytest
 
-from app import app
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+from app import app
 
 @pytest.fixture
 def client():
     with app.test_client() as client:
         yield client
 
+def test_index_page(client):
+    rv = client.get('/')
+    assert rv.status_code == 200
+    assert b'Choose a Country' in rv.data
 
-def test_home(client):
-    """Test the home route"""
-    response = client.get('/')
+def test_post_country(client):
+    rv = client.post('/', data={'country': 'Egypt'})
+    assert rv.status_code == 200
+    assert b'Time and Weather in Cairo, Egypt' in rv.data
 
-    assert response.status_code == 200
-    assert 'Weather App' in response.data.decode()
+def test_invalid_country(client):
+    rv = client.post('/', data={'country': 'InvalidCountry'})
+    assert rv.status_code == 200
+    assert b'Time and Weather in' not in rv.data
